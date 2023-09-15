199a57f792826d0ae8806b47bed435dc9a0927e9
Muhammad Ajmal <Ajmal@zicronlabs.com>
2023-09-15 05:25:09 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState("Click the button to generate a quote!");
+
+  const quotes = [
+    "This is a simple quote.",
+    "Life is what happens when you're busy making other plans.",
+    "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    "The only limit to our realization of tomorrow will be our doubts of today."
+  ];
+
+  const generateQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="quote">{quote}</div>
+        <button onClick={generateQuote} className="generate-btn">Generate Quote</button>
+      </div>
     </div>
   );
 }
